19a7b51bb6b2e64bc362c1e3e5ce82cd7bdcde9f
KUNAL <KUNAL@DESKTOP-4SS5V58>
2025-07-19 17:18:30 +0530

    re-run failed test-cases commit

---
+++ src/main/java/com/mystore/utility/RetryAnalyzer.java
@@ -0,0 +1,20 @@
+package com.mystore.utility;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private final int maxRetryCount = 2; // Total 3 attempts (1 original + 2 retries)
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount) {
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+
+}
